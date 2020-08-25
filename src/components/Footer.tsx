8939384357bcf591c687196e6834cc95173a4b1c
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-08-25 16:02:50 -0600

    Remove gradient from menu icon. Add footer columns.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,52 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 // import { Nav, Navbar } from "react-bootstrap";
 
 const FooterElement: React.FC = () => {
-  return <div>Lorem ipsum dolor sit amet.</div>;
+  return (
+    <div className="main-footer">
+      <Container>
+        <Row>
+          <Col>
+            <h4>Lorem, ipsum.</h4>
+            <ul className="list-unstyled">
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+            </ul>
+          </Col>
+          <Col>
+            <h4>Lorem, ipsum.</h4>
+            <ul className="list-unstyled">
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+            </ul>
+          </Col>
+          <Col>
+            <h4>Lorem, ipsum.</h4>
+            <ul className="list-unstyled">
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+            </ul>
+          </Col>
+          <Col>
+            <h4>Lorem, ipsum.</h4>
+            <ul className="list-unstyled">
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+              <li>Lorem, ipsum.</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default FooterElement;
