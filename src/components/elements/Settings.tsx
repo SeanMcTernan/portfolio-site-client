2cb50ad7028b2293741cd396e3642b1342469824
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-10-08 17:55:17 -0600

    Render button state depending on DB selection

--- src/components/elements/Settings.tsx
+++ src/components/elements/Settings.tsx
@@ -1,5 +1,4 @@
-import { API } from "aws-amplify";
-import React, { useState } from "react";
+import React from "react";
 import {
   Modal,
   Button,
@@ -9,8 +8,6 @@ import {
   OverlayTrigger,
   Tooltip,
 } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
-import onError from "../../libs/errorLib";
 import "../../styles/Settings.css";
 
 interface Props {
@@ -18,18 +15,11 @@ interface Props {
   show: boolean;
   onHide: () => void;
 }
-interface Values {
-  references: boolean;
-  hiddenrepos: boolean;
-  resume: boolean;
-  latest: boolean;
-}
 
-const Settings: React.FC<Props> = (props) => {
+const Settings: React.FC<Props> = ({ ...props }) => {
   const logProps = (permissions: any) => {
     console.log(permissions);
   };
-  const [isLoading, setIsLoading] = useState(false);
   const renderTooltip = (props: any) => (
     <Tooltip id="button-tooltip" {...props}>
       Click to request access
@@ -37,83 +27,101 @@ const Settings: React.FC<Props> = (props) => {
   );
 
   return (
-    <Modal
-      backdrop="static"
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header className="settingsContent" closeButton>
-        <Modal.Title
-          id="contained-modal-title-vcenter"
-          className="settingsContent"
+    <>
+      {!props.permissions ? null : (
+        <Modal
+          backdrop="static"
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
         >
-          Settings:
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="settingsContent show-grid">
-        <Container>
-          <p className="settingsParagraph">
-            To request access to any items below, please click on the
-            corresponding buttons:
-          </p>
-          <Row className="settingsRows">
-            <Col xs={12} md={8} lg={4}>
-              View Refences:
-            </Col>
-            <Col xs={12} md={8} lg={4}>
-              <OverlayTrigger
-                placement="right"
-                delay={{ show: 150, hide: 250 }}
-                overlay={renderTooltip}
-              >
-                <Button
-                  size="sm"
-                  variant="info"
-                  onClick={() => {
-                    logProps(props.permissions.latest);
-                  }}
-                >
-                  Request
-                </Button>
-              </OverlayTrigger>
-            </Col>
-          </Row>
-          <Row className="settingsRows">
-            <Col xs={12} md={8} lg={4}>
-              View Hidden Repositories:
-            </Col>
-            <Col xs={12} md={8} lg={4}>
-              <OverlayTrigger
-                placement="right"
-                delay={{ show: 250, hide: 400 }}
-                overlay={renderTooltip}
-              >
-                <Button size="sm" variant="info">
-                  Request
-                </Button>
-              </OverlayTrigger>
-            </Col>
-          </Row>
-          <Row className="settingsRows">
-            <Col xs={12} md={8} lg={4}>
-              Access Downloadable Resume:
-            </Col>
-            <Col xs={12} md={8} lg={4}>
-              <Button size="sm" disabled variant="success">
-                Approved
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      </Modal.Body>
-      <Modal.Footer className="settingsContent">
-        <Button variant="info" onClick={props.onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
+          <Modal.Header className="settingsContent" closeButton>
+            <Modal.Title
+              id="contained-modal-title-vcenter"
+              className="settingsContent"
+            >
+              Settings:
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="settingsContent show-grid">
+            <Container>
+              <p className="settingsParagraph">
+                To request access to any items below, please click on the
+                corresponding buttons:
+              </p>
+              <Row className="settingsRows">
+                <Col xs={12} md={8} lg={4}>
+                  View Refences:
+                </Col>
+                <Col xs={12} md={8} lg={4}>
+                  <OverlayTrigger
+                    placement="right"
+                    delay={{ show: 150, hide: 250 }}
+                    overlay={renderTooltip}
+                  >
+                    <Button
+                      size="sm"
+                      variant="info"
+                      onClick={() => {
+                        logProps(props.permissions.resume);
+                      }}
+                    >
+                      Request
+                    </Button>
+                  </OverlayTrigger>
+                </Col>
+              </Row>
+              <Row className="settingsRows">
+                <Col xs={12} md={8} lg={4}>
+                  View Hidden Repositories:
+                </Col>
+                <Col xs={12} md={8} lg={4}>
+                  <OverlayTrigger
+                    placement="right"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderTooltip}
+                  >
+                    <Button size="sm" variant="info">
+                      Request
+                    </Button>
+                  </OverlayTrigger>
+                </Col>
+              </Row>
+              <Row className="settingsRows">
+                <Col xs={12} md={8} lg={4}>
+                  Access Downloadable Resume:
+                </Col>
+                <Col xs={12} md={8} lg={4}>
+                  {props.permissions.resume ? (
+                    <Button size="sm" disabled variant="outline-warning">
+                      Requested
+                    </Button>
+                  ) : (
+                    [
+                      props.permissions.latest ? (
+                        <Button size="sm" variant="info">
+                          Request
+                        </Button>
+                      ) : (
+                        <Button size="sm" disabled variant="success">
+                          Approved
+                        </Button>
+                      ),
+                    ]
+                  )}
+                </Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer className="settingsContent">
+            <Button variant="info" onClick={props.onHide}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </>
   );
 };
 
