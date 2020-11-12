e659bd73d07312ebdc9e5dddd816e26d5782a3cf
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-11-11 18:02:33 -0700

    Add drag and drop project info

--- src/libs/projectData.ts
+++ src/libs/projectData.ts
@@ -70,6 +70,18 @@ const projectInfo = [
     img: require('../images/NoteTaking-App.jpg'),
     maps:
       'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
+  },
+  {
+    id: 7,
+    headerTitle: 'TS Drag & Drop Project',
+    headerSubTitle: 'soluta numquam',
+    headerText: 'A small Typescript project, to explore drag and drop functionality within a browser.',
+    title: 'Molestias officia excepturi',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?',
+    img: require('../images/Drag&Drop.gif'),
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
   }
 ];
 
