46ff652c605e32a7dfb621de24e23c9dc864786a
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-11-11 17:42:32 -0700

    Update Portfolio Site Client backend info

--- src/libs/projectData.ts
+++ src/libs/projectData.ts
@@ -37,13 +37,13 @@ const projectInfo = [
   },
   {
     id: 4,
-    headerTitle: 'RiverText',
+    headerTitle: 'Portfolio Site - Backend',
     headerSubTitle: 'soluta numquam',
-    headerText: 'River text is a system designed to help get river level updates via the delorme satelite network. Using data pulled from the government of Canada website, and accurate reading is sent directly to the satelite phone.',
+    headerText: 'The backend of the Portfolio site serves to assist login and permissions on my portfolio site. It allows users to create accounts, login and request permissions to see restricted areas on the site that are otherwise invisible unless approved.',
     title: 'Molestias officia excepturi',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?',
-    img: require('../images/RiverText.jpg'),
+    img: require('../images/PortfolioSite-Backend.jpg'),
     maps:
       'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
   },
