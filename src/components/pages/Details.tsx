bab2b49a9a51a080213e7aad27797495d6aad686
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-11-18 17:14:30 -0700

    Adjust layout on details page

--- src/components/pages/Details.tsx
+++ src/components/pages/Details.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import { ReactComponent as Github } from "../../svgs/github.svg";
 import { InfoConsumer } from "../../libs/contextLib";
+import "../../styles/Details.css";
 
 const Details: React.FC = (props) => {
   return (
@@ -18,71 +19,74 @@ const Details: React.FC = (props) => {
           maps,
         } = value.projectInfo![0];
         return (
-          <Fragment>
-            {/* Project text details */}
-            <Container className="homeDiv align-items-center">
-              <h1
-                style={{ textAlign: "center" }}
-                className="display-1. font-weight-bold"
-              >
-                {headerTitle}
-              </h1>
-              <h4 className="display-5">{headerSubTitle}</h4>
-              <p>{headerText}</p>
-            </Container>
-            {/* GitHub Links */}
-            <Container
-              style={{ paddingBottom: "3vh", textAlign: "center" }}
-              className="mt 5 "
-            >
-              <a
-                href="https://github.com/SeanMcTernan"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github
-                  className="socialIcons"
-                  style={{
-                    width: "5vmin",
-                  }}
-                />
-              </a>
-            </Container>
-            {/* Nav Links */}
-            <Container style={{ paddingBottom: "20vh" }}>
-              <Tabs
-                defaultActiveKey="project-breakdown"
-                id="details-tab"
-                className="mb 25"
-              >
-                <Tab eventKey="project-breakdown" title="Project Breakdown">
-                  <h2 className="mb-3">{title}</h2>
-                  <p>{description}</p>
-                  <img
-                    src={img}
-                    alt={title}
-                    className="img-thumbnail img-fluid"
-                  />
-                </Tab>
-                <Tab
-                  eventKey="project-technologies"
-                  title="Project Technologies"
+          <div className="details">
+            <Fragment>
+              {/* Project text details */}
+              <Container className="homeDiv align-items-center">
+                <h1
+                  style={{ textAlign: "center" }}
+                  className="display-1. font-weight-bold details"
                 >
-                  <h2 className="mb-3">{title}</h2>
-                  <p>{description}</p>
-                  <img
-                    src={img}
-                    alt={title}
-                    className="img-thumbnail img-fluid"
+                  {headerTitle}
+                </h1>
+                <h4 className="display-5">{headerSubTitle}</h4>
+                <p>{headerText}</p>
+              </Container>
+              {/* GitHub Links */}
+              <Container
+                style={{ paddingBottom: "3vh", textAlign: "center" }}
+                className="mt 5 "
+              >
+                <a
+                  href="https://github.com/SeanMcTernan"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github
+                    className="socialIcons"
+                    style={{
+                      textShadow: "2px",
+                      width: "5vmin",
+                    }}
                   />
-                </Tab>
-                <Tab eventKey="motivations" title="Project Technologies">
-                  <h2 className="mb-3">Install Instructions</h2>
-                  <p>{description}</p>
-                </Tab>
-              </Tabs>
-            </Container>
-          </Fragment>
+                </a>
+              </Container>
+              {/* Nav Links */}
+              <Container style={{ paddingBottom: "20vh" }}>
+                <Tabs
+                  defaultActiveKey="project-breakdown"
+                  id="details-tab"
+                  className="mb 25"
+                >
+                  <Tab eventKey="project-breakdown" title="Project Breakdown">
+                    <h2 className="mb-3">{title}</h2>
+                    <p>{description}</p>
+                    <img
+                      src={img}
+                      alt={title}
+                      className="img-thumbnail img-fluid"
+                    />
+                  </Tab>
+                  <Tab
+                    eventKey="project-technologies"
+                    title="Project Technologies"
+                  >
+                    <h2 className="mb-3">{title}</h2>
+                    <p>{description}</p>
+                    <img
+                      src={img}
+                      alt={title}
+                      className="img-thumbnail img-fluid"
+                    />
+                  </Tab>
+                  <Tab eventKey="motivations" title="Project Technologies">
+                    <h2 className="mb-3">Install Instructions</h2>
+                    <p>{description}</p>
+                  </Tab>
+                </Tabs>
+              </Container>
+            </Fragment>
+          </div>
         );
       }}
     </InfoConsumer>
