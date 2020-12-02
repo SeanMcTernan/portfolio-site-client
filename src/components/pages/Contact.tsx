362dc86435d37f7436675fbb29c29d9f04e660a1
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2020-12-02 07:09:34 -0700

    Add H3 to rendered form

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -7,12 +7,11 @@ import {
   FormControl,
   FormLabel,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { FcCheckmark } from "react-icons/fc";
 import LoaderButton from "../elements/LoaderButton";
 import onError from "../../libs/errorLib";
 import useFormFields from "../../libs/hooksLib";
-import "../../styles/ResetPassword.css";
+import "../../styles/Contact.css";
 
 const Contact: React.FC = () => {
   const [emailSent, setEmailSent] = useState(false);
@@ -48,68 +47,84 @@ const Contact: React.FC = () => {
 
   const renderRequestCodeForm = () => {
     return (
-      <form
-        style={{ color: "var(--mainWhite)" }}
-        onSubmit={handleSendCodeClick}
-      >
-        <FormGroup controlId="name">
-          <FormLabel className="font-weight-lighter">Name</FormLabel>
-          <FormControl
-            autoFocus
-            type="name"
-            value={fields.name}
-            style={{ marginBottom: "1vh" }}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="email">
-          <FormLabel className="font-weight-lighter">Email</FormLabel>
-          <FormControl
-            type="email"
-            value={fields.email}
-            style={{ marginBottom: "1vh" }}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="phone">
-          <FormLabel className="font-weight-lighter">Phone</FormLabel>
-          <FormControl
-            type="phone"
-            value={fields.phone}
-            style={{ marginBottom: "1vh" }}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="message">
-          <FormLabel className="font-weight-lighter">Message</FormLabel>
-          <FormControl
-            size="lg"
-            as="textarea"
-            rows={5}
-            type="message"
-            value={fields.message}
-            style={{ marginBottom: "1vh" }}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <LoaderButton
-          block
-          type="submit"
-          bsSize="large"
-          isLoading={isSendingEmail}
-          disabled={!validateCodeForm()}
+      <div>
+        <h3
+          className="font-weight-lighter"
+          style={{ color: "var(--mainWhite)" }}
         >
-          Send Message
-        </LoaderButton>
-      </form>
+          Get in touch
+        </h3>
+        <form
+          style={{ color: "var(--mainWhite)" }}
+          onSubmit={handleSendCodeClick}
+        >
+          <FormGroup controlId="name">
+            <FormLabel className="font-weight-lighter">Name</FormLabel>
+            <FormControl
+              autoFocus
+              type="name"
+              value={fields.name}
+              style={{ marginBottom: "1vh" }}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="email">
+            <FormLabel className="font-weight-lighter">Email</FormLabel>
+            <FormControl
+              type="email"
+              value={fields.email}
+              style={{ marginBottom: "1vh" }}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="phone">
+            <FormLabel className="font-weight-lighter">Phone</FormLabel>
+            <FormControl
+              type="phone"
+              value={fields.phone}
+              style={{ marginBottom: "1vh" }}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="message">
+            <FormLabel className="font-weight-lighter">Message</FormLabel>
+            <FormControl
+              size="lg"
+              as="textarea"
+              rows={5}
+              type="message"
+              value={fields.message}
+              style={{ marginBottom: "1vh" }}
+              onChange={handleFieldChange}
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isSendingEmail}
+            disabled={!validateCodeForm()}
+          >
+            Send Message
+          </LoaderButton>
+        </form>
+      </div>
     );
   };
 
   const renderSuccessMessage = () => {
     return (
-      <div className="success" style={{ color: "var(--mainWhite)" }}>
-        <FcCheckmark className="checkMark" />
-        <p>Your message has been sent! I'll be in touch soon.</p>
+      <div>
+        <p style={{ textAlign: "center" }}>
+          <FcCheckmark
+            style={{
+              fontSize: "60px",
+            }}
+          />
+          <h3 className="font-weight-lighter">
+            Your message has been sent! I'll be in touch soon.
+          </h3>
+        </p>
       </div>
     );
   };
@@ -121,12 +136,6 @@ const Contact: React.FC = () => {
       <Row style={{ paddingBottom: "20vh" }}>
         <Col />
         <Col lg={6}>
-          <h4
-            className="font-weight-lighter"
-            style={{ color: "var(--mainWhite)" }}
-          >
-            Get in touch
-          </h4>
           <div>
             {!emailSent ? renderRequestCodeForm() : renderSuccessMessage()}
           </div>
